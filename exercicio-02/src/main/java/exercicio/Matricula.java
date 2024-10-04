114c2f49a40769098005a072106d97e4d1bbf208
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2024-10-04 20:48:47 -0300

    Add Matricula.java

---
+++ exercicio-02/src/main/java/exercicio/Matricula.java
@@ -0,0 +1,15 @@
+
+package exercicio;
+
+
+public class Matricula {
+    private int numero;
+    private Aluno aluno;
+    private Turma turma;
+    
+    Matricula(int numero, Aluno aluno, Turma turma) {
+        this.numero = numero;
+        this.aluno = aluno;
+        this.turma = turma;
+    }
+}
