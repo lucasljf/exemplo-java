d47be2b16f530efb50165c53d5940fc714f4fe0d
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2024-10-04 20:40:09 -0300

    Add Curso.java

---
+++ exercicio-02/src/main/java/exercicio/Curso.java
@@ -0,0 +1,13 @@
+
+package exercicio;
+
+
+public class Curso {
+    private String nome;
+    private int duracao;
+    
+    Curso(String n, int d) {
+        this.nome = n;
+        this.duracao = d;
+    }
+}
