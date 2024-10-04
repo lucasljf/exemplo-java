b27136efff3a644801a6da4b39e838abf2a61079
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2024-10-04 20:37:30 -0300

    Add Aluno.java

---
+++ exercicio-02/src/main/java/exercicio/Aluno.java
@@ -0,0 +1,14 @@
+package exercicio;
+
+
+public class Aluno {
+    private int codigo;
+    private String nome;
+    private String endereco;
+    
+    
+    Aluno(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+}
