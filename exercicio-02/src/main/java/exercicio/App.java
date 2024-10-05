6bd527d0aa66525081c9e633c87d0591b063e262
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2024-10-04 21:44:46 -0300

    Teste das classes criadas

--- exercicio-02/src/main/java/exercicio/App.java
+++ exercicio-02/src/main/java/exercicio/App.java
@@ -2,6 +2,23 @@ package exercicio;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Oiii....");
+        //instanciar todas as classes criadas
+            Aluno a1 = new Aluno(100, "Fulano");
+            Aluno a2 = new Aluno(200, "Ciclano");
+            
+            a1.setNome("Lucas");
+            
+            Curso c1 = new Curso("Sistemas", 8);
+            Curso c2 = new Curso("Zootecnia", 10);
+            
+            Turma t1 = new Turma("2o periodo", c1);
+            
+            Matricula m1 = new Matricula(20241, a1, t1);
+            Matricula m2 = new Matricula(20242, a2, t1);
+        
+        //imprimir tudo que conseguir
+        System.out.println(m1.getAluno().getNome());
+        System.out.println(m1.getTurma().getDescricao());
+        System.out.println(m1.getTurma().getCurso().getNome());
     }
 }
