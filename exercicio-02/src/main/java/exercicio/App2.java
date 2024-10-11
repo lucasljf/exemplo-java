375448159b94415214ebf14e53fe6fef6fd92839
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2024-10-11 20:36:10 -0300

    Proposta de exercício com Array

---
+++ exercicio-02/src/main/java/exercicio/App2.java
@@ -0,0 +1,11 @@
+package exercicio;
+
+public class App2 {
+    public static void main(String[] args) {
+        //Implemente a leitura dos dados necessários para criar um array
+        //que armazene 03 Alunos.
+        
+        //Implemente a leitura dos dados necessários para criar um array
+        //que armazene 03 Cursos.
+    }
+}
